36abbdaa9ecb4e6cdb802dbe43612c31bb89afa6
Niel Buys <nbuys@ncomp.co.za>
2018-11-27 10:31:43 +0200

    ipset delete added

--- test.php
+++ test.php
@@ -1,8 +1,13 @@
 <?php
-    $ipsetlist = file_get_contents("evil_ips.ipset");
+
+//// to delete not founds ips from ipset use command "php test.php -f 1"
+//// to delete with zero abuseConfidenceScore ips from ipset use command "php test.php -z 1"
+    $options = getopt("f:z:");
+    $ipsetname = 'evil_ips';
     $abuseipdbAPIKey = '<Abuseipdb API Key here>';
-    $logdays = '120';
+    $logdays = '365';
     $separator = "\r\n";
+    $ipsetlist = file_get_contents($ipsetname . ".ipset");
     $line = strtok($ipsetlist, $separator);
     while ($line !== false) {
         # do something with $line
@@ -19,14 +24,30 @@
             curl_close($ch);
             if (isset($data[0]['ip']))
             {
+                $displayline = '';
                 if ($data[0]['abuseConfidenceScore'] < 100)
                 {
-                    echo $data[0]['ip'] . " Score: " . $data[0]['abuseConfidenceScore']  . " -- found\n";
+                    $displayline .= $data[0]['ip'] . " Score: " . $data[0]['abuseConfidenceScore']  . " -- found";
+                }
+                if ($data[0]['abuseConfidenceScore'] == 0 and isset($options['z']) and $options['z'] == 1)
+                {
+                    exec("ipset del " . $ipsetname . " " . $data[0]['ip']);
+                    $displayline .= ' -- deleted from ipset.';
+                }
+                if ($displayline != '')
+                {
+                    echo $displayline . "\n";
                 }
             }
             else
             {
-                echo $ip . " -- not found\n";            
+                echo $ip . " -- not found";  
+                if (isset($options['f']) and $options['f'] == 1)
+                {
+                    echo ' -- deleted from ipset.';
+                    exec("ipset del " . $ipsetname . " " . $ip);
+                }
+                echo "\n";
             }
         }
         $line = strtok( $separator );
